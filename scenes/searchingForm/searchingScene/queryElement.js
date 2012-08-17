51c0dce10f78bc445b815e3dcd2f649a6dc7eec6
ppanyong <ppanyong@gmail.com>
2012-08-17 14:33:42 +0800

    change事件

--- scenes/searchingForm/searchingScene/queryElement.js
+++ scenes/searchingForm/searchingScene/queryElement.js
@@ -42,18 +42,17 @@
         registerEvents:function(){
             var that = this;
             $(this.rootNode).find('input').blur(function(e){
-                console.log('blur')
                 that.setNucleusAttrs('value',this.value);
             });
             $(this.rootNode).find('input').focus(function(e){
-               that.clearErrorState();
+               that.clearErrorStatus();
             });
         },
         error:function(errMsg){
             $(this.rootNode).addClass('mi-form-item-error');
             $(this.rootNode).find('.mi-form-explain').get(0).innerHTML = errMsg;
         },
-        clearErrorState:function(){
+        clearErrorStatus:function(){
             $(this.rootNode).removeClass('mi-form-item-error');
             $(this.rootNode).find('.mi-form-explain').get(0).innerHTML =this.formExplain;
         }
@@ -63,9 +62,12 @@
      * 日历输入
      * 他的结构本身包含了一个预定义好的nucleus，按照固定日期格式进行校验
      * 所以，不必再设置一个nucleus实例了
+     * Calender的rootNode将指向input本身，目的是为了后续双日历或者三日历控件的组合使用
      */
     cellula.QueryElement.Calendar = new cellula.Class('QueryElement.Calendar',{
         formExplain:'请填写',
+        status:true,
+        container:false,
         regCal : /^\d{4}-((0[1-9]{1})|(1[0-2]{1}))-((0[1-9]{1})|([1-2]{1}[0-9]{1})|(3[0-1]{1}))$/,
         init:function(cfg){
             var that = this;
@@ -82,57 +84,92 @@
         },
         render:function(){
             this._super();
+            var input =  $(this.rootNode).get(0);
+            var inputElV =  input.value;
+            if(inputElV) this.setNucleusAttrs('value',inputElV);
+            var inputV = this.getNucleus().get('value');
+            input.value = (inputV)?inputV:'';
+            this.container = $(this.rootNode).parent('.mi-form-item').get(0);
+
 
         },
         registerEvents:function(){
-            this._super();
+            var that = this;
+            $(this.rootNode).blur(function(e){
+                that.setNucleusAttrs('value',this.value);
+            });
+            $(this.rootNode).focus(function(e){
+               that.clearErrorStatus();
+            });
             //todo:use Calender to input date value
+        },
+        error:function(errMsg){
+            if(this.container){
+                $(this.container).addClass('mi-form-item-error');
+                $(this.container).find('.mi-form-explain').get(0).innerHTML = errMsg;
+            }
+            this.status = false;
+        },
+        clearErrorStatus:function(){
+             if(this.container){
+                 $(this.container).removeClass('mi-form-item-error');
+                 $(this.container).find('.mi-form-explain').get(0).innerHTML =this.formExplain;
+             }
+             this.status = true;
         }
-    }).inherits(cellula.QueryElement.Input);
+    }).inherits(cellula.QueryElement);
 
     cellula.QueryElement.DoubleCalendar = new cellula.Class('QueryElement.DoubleCalendar',{
         formExplain:'请填写',
         startCalender:null,
         endCalender:null,
         init:function(cfg){
-            
+            var tmpNuCl = new cellula.Class({
+                startCalenderNucleus:null,
+                endCalenderNucleus:null,
+                validate:function(data,el){
+                   
+                }
+            }).inherits(cellula.Nucleus);
             this._super(cfg);
-            //todo:这里结构问题会导致出错，root应该是指向容器。
-            startCalender = new cellula.QueryElement.Calendar({
+            this.startCalender = new cellula.QueryElement.Calendar({
                 rootNode:$(cfg.startCalenderRoot)
-            })
-            console.log(cfg.startCalenderRoot)
-            endCalender = new cellula.QueryElement.Calendar({
+            });
+            this.endCalender = new cellula.QueryElement.Calendar({
                 rootNode:$(cfg.endCalenderRoot)
-            })
+            });
+            this.startCalender.getNucleus().on('change',this.changeHandler,this);
+            this.endCalender.getNucleus().on('change',this.changeHandler,this);
+            this._nucleus = new tmpNuCl({startCalenderNucleus:this.startCalender.getNucleus(),endCalenderNucleus:this.endCalender.getNucleus()});
         },
         render:function(){
             this._super();
-            var input1 =  $(this.rootNode).find('input').get(0);
-            var input2 =  $(this.rootNode).find('input').get(1);
-            var inputElV1 =  input1.value;
-            var inputElV2 =  input2.value;
-            var inputElV = {};
-            if(inputElV1 && inputElV2){
-                inputElV.date1 = inputElV1;
-                inputElV.date2 = inputElV2;
-                this.setNucleusAttrs('value',inputElV);
-            }
-
-            var inputV = this.getNucleus().get('value');
-            input1.value = (inputV.date1)?inputV.date1:'';
-            input2.value = (inputV.date2)?inputV.date2:'';
-
         },
         registerEvents:function(){
             this._super();
             //todo:use Calender to input date value
+            var that = this;
+
+
+        },
+        changeHandler:function(attrs){
+            console.log('change');
+            if(this.startCalender.status && this.endCalender.status ){
+                var d1 = this.startCalender.getNucleus().get('value');
+                var d2 = this.endCalender.getNucleus().get('value');
+                console.log(d1);
+                if(new Date(d1) > new Date(d2)){
+                    this.trigger('error',that);
+                    console.log('开始时间不能晚于结束时间');
+                    this.error('开始时间不能晚于结束时间');
+                }
+            }
         },
         error:function(errMsg){
             $(this.rootNode).addClass('mi-form-item-error');
             $(this.rootNode).find('.mi-form-explain').get(0).innerHTML = errMsg;
         },
-        clearErrorState:function(){
+        clearErrorStatus:function(){
             $(this.rootNode).removeClass('mi-form-item-error');
             $(this.rootNode).find('.mi-form-explain').get(0).innerHTML =this.formExplain;
         }
